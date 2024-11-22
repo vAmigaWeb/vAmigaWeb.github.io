f5667a5be8aa9dd473704e69193ed8ba0cb5acc3
mithrendal <mithrendal@web.de>
2024-11-22 12:50:16 +0100

    warp animation

--- js/vAmiga_ui.js
+++ js/vAmiga_ui.js
@@ -476,10 +476,14 @@ function message_handler_queue_worker(msg, data, data2)
         $("#button_ff").html(
             is_warping ?
             `
-<svg xmlns="http://www.w3.org/2000/svg" width="1.6em" height="1.6em" fill="currentColor" class="bi bi-fast-forward-fill" viewBox="0 0 16 16">
+<svg xmlns="http://www.w3.org/2000/svg"  width="1.6em" height="1.6em" fill="currentColor" class="bi bi-fast-forward-fill" viewBox="0 0 16 16">
   <path d="M7.596 7.304a.802.802 0 0 1 0 1.392l-6.363 3.692C.713 12.69 0 12.345 0 11.692V4.308c0-.653.713-.998 1.233-.696z"/>
   <path d="M15.596 7.304a.802.802 0 0 1 0 1.392l-6.363 3.692C8.713 12.69 8 12.345 8 11.692V4.308c0-.653.713-.998 1.233-.696z"/>
-</svg>`:
+</svg>
+<div style="position:absolute;bottom:-1px;font-size:x-small;
+animation: blink 1s infinite">warp</div>
+`
+:
 `
 <svg xmlns="http://www.w3.org/2000/svg" width="1.6em" height="1.6em" fill="currentColor" class="bi bi-fast-forward" viewBox="0 0 16 16">
           <path d="M6.804 8 1 4.633v6.734zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C.713 12.69 0 12.345 0 11.692V4.308c0-.653.713-.998 1.233-.696z"/>
